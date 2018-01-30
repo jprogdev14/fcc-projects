05656d766184980557ef2c32d10b5162a62f0715
James Viviers <jprogdev14@gmail.com>
2018-01-30 05:08:26 +0200

    Finished Leaderboard

---
+++ Data-visualization/camper-leaderboard/src/header/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './header.css';
+
+export default class Header extends Component {
+    render() {
+        return(
+            <header>
+                <h1>FreeCodeCamp Leaderboard</h1>
+                <h2>Developed by James Viviers</h2>
+            </header>
+        );
+    }
+}
